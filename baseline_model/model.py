c6226755f99fa8be55a504f49bff11a0be50647e
viktoriaschneider <viktoria.schneider@onespire.hu>
2026-04-21 16:55:22 +0200

    flag5

--- baseline_model/model.py
+++ baseline_model/model.py
@@ -21,7 +21,6 @@ def get_model():
         age = X[:, [34]]
         resp = X[:, [6]]
         lactate = X[:, [22]]
-        o2sat_low= o2sat < 90
         lactate_high = lactate > 2
         o2sat_fio2_ratio = o2sat/(fio2 + 1e-6) 
         map_age_ratio = map/(age + 1e-6)
@@ -30,7 +29,7 @@ def get_model():
         shock_index = hr / (sbp + 1e-6)
         bun_creat_ratio = bun / (creat + 1e-6)
         return np.hstack([X, shock_index, bun_creat_ratio, o2sat_fio2_ratio, map_age_ratio, hr_temp, resp_lactate, 
-                          lactate_high, o2sat_low])
+                          lactate_high])
  
     model = Pipeline(
         [
